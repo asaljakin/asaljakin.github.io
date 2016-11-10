b9dcb51826120209470cac027579cf8fc2bdf96e
asaljakin <asaljakin@gmail.com>
2016-11-10 17:16:04 +0200

    Lesson5 add

--- Lesson5/Lesson5.js
+++ Lesson5/Lesson5.js
@@ -6,17 +6,13 @@ var heightRocks = [2,1,5,0,3,4,7,2,3,1,0];
 
 function calculate(heightRocks){
     var maxHead =heightRocks.shift(), maxTail = heightRocks.pop(), rez = 0;
-
+    console.log("Height rocks: "+heightRocks);
     while (heightRocks.length >0) {
         var nextHead = (heightRocks.shift());
         var nextTail = (heightRocks.pop());
 
         if (nextHead === undefined) { nextHead = maxHead};
         if (nextTail === undefined) { nextTail = maxTail};
-
-        console.log("Rocks: "+heightRocks);
-        console.log("nextHead: "+nextHead+"  maxHead: "+maxHead);
-        console.log("nextTail: "+nextTail+"  maxTail: "+maxTail);
 
         if (nextHead>maxHead){
             maxHead = nextHead;
